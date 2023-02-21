e8f374501b49b4acd9a59662f9ec6737cb72f74b
Brian Hughes <b7rian@gmail.com>
2023-02-20 19:31:50 -0500

    WIP

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,35 @@ module Main (main) where
 
 import Lib
 
-data Units = BinaryUnits | DecimalUnits
-data BinaryUnits = Chars | Words | Blocks | Kibibytes | Mebibytes | Tebibytes
-data DecimalUnits = Kilobytes | Megabytes | Terabytes
+
+-- From the dd man page:
+-- N and BYTES may be followed by the following multiplicative
+-- suffixes: c=1, w=2, b=512, kB=1000, K=1024, MB=1000*1000,
+-- M=1024*1024, xM=M, GB=1000*1000*1000, G=1024*1024*1024, and so on
+-- for T, P, E, Z, Y.  Binary prefixes can be used, too: KiB=K, MiB=M,
+-- and so on.  If N ends in 'B', it counts bytes not blocks.
+
+data Units =
+  Chars
+  | Words
+  | Blocks
+  | Kibibytes
+  | Mebibytes
+  | Tebibytes
+  | Kilobytes
+  | Megabytes
+  | Terabytes
+
+instance Show Units where
+  show Chars = "c"
+  show Words = "w"
+  show Blocks = "b"
+  show Kibibytes = "K"
+  show Mebibytes = "M"
+  show Tebibytes = "T"
+  show Kilobytes = "KB"
+  show Megabytes = "MB"
+  show Terabytes = "TB"
 
 main :: IO ()
 main = putStrLn "Foo"
